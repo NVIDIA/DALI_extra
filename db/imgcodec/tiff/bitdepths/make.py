cb09f2e149b65d85de18a2aff776d8e2c989c8cf
Szymon Karpiński <34919255+szkarpinski@users.noreply.github.com>
2022-09-01 14:52:58 +0200

    Add test images for all bitdepths (#99)
    Signed-off-by: Szymon Karpiński <skarpinski@nvidia.com>

--- db/imgcodec/tiff/bitdepths/make.py
+++ db/imgcodec/tiff/bitdepths/make.py
@@ -1,6 +1,7 @@
 import os
 import tifffile
 import numpy as np
+import tempfile
 
 dali_extra_path = os.environ['DALI_EXTRA_PATH']
 tiff_path = os.path.join(dali_extra_path, 'db/imgcodec/tiff/bitdepths')
@@ -17,7 +18,23 @@ def filename(bits):
 def ref_filename(bits):
     return os.path.join(ref_path, f'rgb_{bits}bit.tiff.npy')
 
-for bits in (8, 16, 32):
-    uint_image = (image * (2**bits - 1)).astype(f'uint{bits}')
-    tifffile.imwrite(filename(bits), uint_image)
-    np.save(ref_filename(bits), uint_image)
+def ref_float_filename(bits):
+    return os.path.join(ref_path, f'rgb_{bits}bit_float.tiff.npy')
+
+standard_bitdepths = [8, 16, 32]
+
+for bits in range(1, 32+1):
+    ref = np.round(image * (2**bits - 1)) / (2**bits - 1)
+    np.save(ref_float_filename(bits), ref)
+    if bits in standard_bitdepths:
+        # For standard bit-depths it's possible to test for exact match
+        np.save(ref_filename(bits), (image * (2**bits - 1)).astype(f'uint{bits}'))
+
+    if bits in standard_bitdepths:
+        # For images in standard bit-depths save the image as is
+        tifffile.imwrite(filename(bits), (image * (2**bits - 1)).astype(f'uint{bits}'))
+    else:
+        # For non-standard bit-depths make a 32-bit image and convert it.
+        tmp = tempfile.NamedTemporaryFile()
+        tifffile.imwrite(tmp, (ref * (2**32 - 1)).astype('uint32'))
+        os.system(f'convert -type TrueColor -define tiff:bits-per-sample={bits} -depth {bits} {tmp.name} {filename(bits)}')
